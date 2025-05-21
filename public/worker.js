1d20531a685b73df60e08a075025aff904204b70
isitfall <pdand94@gmail.com>
2025-05-21 17:52:52 +0300

    Revert "websockets"
    This reverts commit 95bfe838a2605d38f4823845514a46e107b75f80.

--- public/worker.js
+++
@@ -1,9 +0,0 @@
-const socket = new WebSocket('ws://localhost:3000');
-
-socket.addEventListener('message', (event) => {
-    postMessage(event.data);
-});
-
-socket.addEventListener('open', () => {
-    console.log('Worker connected to WebSocket');
-});
